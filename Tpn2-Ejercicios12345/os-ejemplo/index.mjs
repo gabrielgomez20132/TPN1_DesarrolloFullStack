f08cb54be4b000607663e4d317cebda6c2da2dc3
gabrielgomez20132 <gabrielgomez_100@hotmail.com>
2024-10-17 10:33:04 -0300

    os-ejemplo

---
+++ Tpn2-Ejercicios12345/os-ejemplo/index.mjs
@@ -0,0 +1,12 @@
+import os from 'os';
+
+console.log('Arquitectura:', os.arch());
+
+console.log('Plataforma:', os.platform());
+
+console.log('Memoria total:', os.totalmem());
+
+console.log('Memoria Libre:', os.freemem());
+
+
+console.log('Informacion de la CPU:', os.cpus());
